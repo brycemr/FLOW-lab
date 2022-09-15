925ae3ea8a7ab3caa70e9b56cd4051e72e8021ed
brycemr <brycerichard26@gmail.com>
2022-09-15 08:48:14 -0600

    Update JensenWake.jl

--- JensenOptimization/JensenWake.jl
+++ JensenOptimization/JensenWake.jl
@@ -5,22 +5,6 @@ gr()
 
 """
 DownstreamVelocity(u,r0,x)
-u - ambient wind velocity 
-r0 - rotor radius
-x - distance downstream
-return v - velocity in the wake at distance x from generator
-
-This function applies the Jensen wake model to a single turbine to
-calculate the downstream velocity at a given distance. It assumes that
-alpha (wake entrainment constant) is equal to 0.1
-"""
-function DownstreamVelocity(u,r0,x)
-    v = u*(1- ((2/3)*((r0/(r0+0.1*x))^2)))
-    return v
-end
-
-"""
-DownstreamVelocity(u,r0,x)
 xr0_ratio - ratio of x(distance downstream) to r0(radius of rotor)
 return velocity ratio - returns ratio of wind velocity to ambient air velocity (v/u)
 
@@ -28,10 +12,6 @@ This function applies the Jensen wake model to a single turbine to
 calculate the downstream velocity ratio at a given distance. It assumes that
 alpha (wake entrainment constant) is equal to 0.1
 """
-function DownstreamVelocityRatio(prevTurbineRatio, xr0_ratio)
-    velocity_ratio = 1 - ((1-(prevTurbineRatio/3))*((1/(1+0.1*xr0_ratio))^2))
-    return velocity_ratio
-end
 
 
 """
@@ -55,6 +35,10 @@ end
 println(velocityTable)
 println(velocityRatioTable)
 """
+function WakeFunction(prevTurbineRatio, xr0_ratio, angle)
+    deficit = (1-(prevTurbineRatio/3))*((1/(1+0.1*xr0_ratio))^2)*((1+cosd(9*angle))/2)
+    return deficit
+end
 
 """
 WindfarmOutputRatio(positions, theta)
@@ -95,8 +79,28 @@ function WindfarmOutputRatio(positions, wind_direction, rotor_radius)
     return output/numTurbines
 end
 
-function WindVelocityInFarm(x,y,positions,theta,rotor_radius,u)
-    return u*WindVelocityRatioInFarm(x,y,positions,theta,rotor_radius)
+"""
+Calcuate the Power Output Ratio (compared to a wind farm with 0 interacting turbines) of a wind farm given the positions for the wind farm, rotor radius for the turbines
+and wind rose data (an array of speed, frequency, and direction)
+"""
+function WindfarmYearPerformance(positions, windData, rotor_radius)
+    #Power = constants*v^3
+    total_output = 0
+    possible_output = 0
+    for i=axes(windData,1)
+        println(windData[i,:])
+        windVelocity = windData[i, 1]
+        yearlyFraction = windData[i, 2]
+        output = (windVelocity^3)*(yearlyFraction)*WindfarmOutputRatio(positions, windData[i, 3], rotor_radius)
+        total_output += output
+        possible = (windVelocity^3)*(yearlyFraction)
+        possible_output += possible
+        ratio = output / possible
+        println("For this wind data the farm acheives $output of $possible for $ratio")
+    end
+    finalRatio = total_output/possible_output
+    println("In a year we acheive $total_output out of $possible_output for $finalRatio")
+    return finalRatio
 end
 
 function FindInteractions(upStreamTurbines,currPosition,rotor_radius)
@@ -115,34 +119,6 @@ function FindInteractions(upStreamTurbines,currPosition,rotor_radius)
     return interactions
 end
 
-function PlotWindfarm(x, y, xs, ys, velocityRatio, theta, rotor_radius)
-    scatter(xs, ys, seriestype=:scatter, title = "Wind Farm Turbine Layout", label="", markershape=:cross, size=(400,400), markerstrokewidth=4)
-    outputRatio = round(velocityRatio, digits=3)
-
-    for i=eachindex(xs)
-        turbineTop = [xs[i]+rotor_radius*cosd(theta+90), ys[i]+rotor_radius*sind(theta+90)]
-        turbineBottom = [xs[i]+rotor_radius*cosd(theta-90), ys[i]+rotor_radius*sind(theta-90)]
-        wakeTop = [turbineTop[1]+100*rotor_radius*cosd(theta), turbineTop[2]+100*rotor_radius*tand(theta+WAKE_ANGLE)] #100*rotor_radius because at x = 100r0 the velocity is more than .99u
-        wakeBottom = [turbineBottom[1]+100*rotor_radius*cosd(theta), turbineBottom[2]+100*rotor_radius*tand(theta-WAKE_ANGLE)] 
-        wakeXs = [wakeBottom[1], turbineBottom[1], turbineTop[1], wakeTop[1]]
-        wakeYs = [wakeBottom[2], turbineBottom[2], turbineTop[2], wakeTop[2]]
-        plot!(wakeXs, wakeYs, linealpha=0.50, label="")
-    end
-
-    scatter!([x],[y], color="red", markershape=:xcross, label="$outputRatio u")
-    tail = [x-50*cosd(theta), x]
-    head = [y-50*sind(theta), y]
-    plot!(tail, head, linealpha=0.50, color="blue", label="", arrow=true)
-    plot!(xlims=(-110,110), framestyle=:none)
-    ylims!((-110,110))
-    
-    filename = "Windfarm_Plot.pdf"
-    savefig(filename)
-end
-
-#REFINED METHODS THAT INCLUDES WAKE EQUATION
-
-#Start on left, find speed at each turbine, if a point is specified, only need to find wind speeds of turbines to the left.
 function VelocityRatioInFarm(x, y, positions, theta, rotor_radius)
     #Convert all positions to new coordinate with wind direction as (+) x-axis
     xs = []
@@ -173,7 +149,7 @@ function VelocityRatioInFarm(x, y, positions, theta, rotor_radius)
     turbineVelocitites, wakeParents = FixedCalculateVelocityRatioAtTurbines(newCoordinates, rotor_radius)
     #println("The velocity ratios in the upstream turbines are $turbineVelocitites")
 
-    velocityRatio = calcRatioAtLocation(pos, newCoordinates, turbineVelocitites, wakeParents, rotor_radius)[1]
+    velocityRatio = VelocityRatioAtLocation(pos, newCoordinates, turbineVelocitites, wakeParents, rotor_radius)[1]
 
     PlotWindfarm(x, y, xs, ys, velocityRatio, theta, rotor_radius)
 
@@ -190,7 +166,7 @@ function FixedCalculateVelocityRatioAtTurbines(positions,rotor_radius)
     wakeParents = [[0]]
 
     for i=2:numTurbines
-        velocityRatio, newParents = calcRatioAtLocation(positions[i], positions[1:i-1], Yn, wakeParents, rotor_radius)
+        velocityRatio, newParents = VelocityRatioAtLocation(positions[i], positions[1:i-1], Yn, wakeParents, rotor_radius)
 
         push!(wakeParents, newParents)
         push!(Yn, velocityRatio)     
@@ -198,13 +174,13 @@ function FixedCalculateVelocityRatioAtTurbines(positions,rotor_radius)
     return Yn, wakeParents
 end
 
-function calcRatioAtLocation(location, upstreamTurbines, upstreamVelocities, wakeParents, rotor_radius)
+function VelocityRatioAtLocation(location, upstreamTurbines, upstreamVelocities, wakeParents, rotor_radius)
     interactions = FindInteractions(upstreamTurbines, location, rotor_radius)
 
     velocityRatio = 1
     
     ignoreParents=Set()
-
+    ##COMMENTS
     for j in interactions
         for parent in wakeParents[j]
             push!(ignoreParents, parent)
@@ -218,13 +194,36 @@ function calcRatioAtLocation(location, upstreamTurbines, upstreamVelocities, wak
     return velocityRatio, interactions
 end
 
-function WakeFunction(prevTurbineRatio, xr0_ratio, angle)
-    deficit = (1-(prevTurbineRatio/3))*((1/(1+0.1*xr0_ratio))^2)*((1+cosd(9*angle))/2)
-    return deficit
+function PlotWindfarm(x, y, xs, ys, velocityRatio, theta, rotor_radius)
+    scatter(xs, ys, seriestype=:scatter, title = "Wind Farm Turbine Layout", label="", markershape=:cross, size=(400,400), markerstrokewidth=4)
+    outputRatio = round(velocityRatio, digits=3)
+
+    for i=eachindex(xs)
+        turbineTop = [xs[i]+rotor_radius*cosd(theta+90), ys[i]+rotor_radius*sind(theta+90)]
+        turbineBottom = [xs[i]+rotor_radius*cosd(theta-90), ys[i]+rotor_radius*sind(theta-90)]
+        wakeTop = [turbineTop[1]+100*rotor_radius*cosd(theta), turbineTop[2]+100*rotor_radius*tand(theta+WAKE_ANGLE)] #100*rotor_radius because at x = 100r0 the velocity is more than .99u
+        wakeBottom = [turbineBottom[1]+100*rotor_radius*cosd(theta), turbineBottom[2]+100*rotor_radius*tand(theta-WAKE_ANGLE)] 
+        wakeXs = [wakeBottom[1], turbineBottom[1], turbineTop[1], wakeTop[1]]
+        wakeYs = [wakeBottom[2], turbineBottom[2], turbineTop[2], wakeTop[2]]
+        plot!(wakeXs, wakeYs, linealpha=0.50, label="")
+    end
+
+    scatter!([x],[y], color="red", markershape=:xcross, label="$outputRatio u")
+    tail = [x-50*cosd(theta), x]
+    head = [y-50*sind(theta), y]
+    plot!(tail, head, linealpha=0.50, color="blue", label="", arrow=true)
+    plot!(xlims=(-110,110), framestyle=:none)
+    ylims!((-110,110))
+    
+    filename = "Windfarm_Plot.pdf"
+    savefig(filename)
 end
+
 
 #INPUTS TO SOLVE FOR
 WAKE_ANGLE = 20
+
+#Create Circle
 circle = []
 circle_radius = 100
 numTurbines = 10
@@ -233,16 +232,28 @@ for i=1:numTurbines
     push!(circle,[circle_radius*cosd((angle*i)+10), circle_radius*sind((angle*i)+10)])
 end
 
+#Create straight line of turbines
+line=[[0,0],[50,0],[100,0],[150,0],[200,0],[250,0],[300,0],[350,0],[400,0],[450,0]]
 
-#INPUTS
 
-positions=[[0,0],[50,0],[100,0],[150,0],[200,0],[250,0],[300,0],[350,0],[400,0],[450,0]]
-println("This farm outputs ")
-#println(VelocityRatioInFarm(97, 13, circle, -5, 10))
-print("Wind farm output: ")
-for i=-18:18
-    print("For angle $i the output ratio is: ")
-    println(WindfarmOutputRatio(circle, i, 10))
-end
-output = VelocityRatioInFarm(180, 0, positions, 45, 5)
+#Create Wind Rose (velocity, fequency, direction)
+windRose = [
+    14 0.1 0;
+    10 0.3 90;
+    5 0.4 180;
+    20 0.2 270]
+
+#Call Functions
+#println("Wind farm output: ")
+#WindfarmYearPerformance(line, windRose, 10)
+
+
+#TEST RANGE OF ANGLES FOR CIRCLE
+#for i=-18:18
+    #print("For angle $i the output ratio is: ")
+    #println(WindfarmOutputRatio(circle, i, 10))
+#end
+
+
+output = VelocityRatioInFarm(0, 0, circle, 30, 10)
 #println("The output of this farm is $output")
